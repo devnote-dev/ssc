238b3788a7b4da5bad68269065c3504a33a1b5fa
devnote-dev <devnote.dev75@gmail.com>
2023-11-01 03:21:55 +0000

    fix(bin): re-quote arguments because Windows

--- bin/ssc.dart
+++ bin/ssc.dart
@@ -4,6 +4,13 @@ import 'package:args/command_runner.dart';
 import 'package:ssc/ssc.dart';
 
 Future<Never> main(List<String> args) async {
+  // because Windows...
+  for (final (index, arg) in args.indexed) {
+    if (arg.contains(' ')) {
+      args[index] = '"$arg"';
+    }
+  }
+
   try {
     exit(await run(args));
   } on UsageException catch (e) {
